9b4e8aa065b1bde2c1436c520f319d83a1829643
malinovvski <m.malinovvski@gmail.com>
2022-09-11 19:52:37 +0200

    T2.The name of JS script

---
+++ 2.8 Basic operators, maths/T1.The postfix and prefix forms.js
@@ -0,0 +1,13 @@
+
+ // Task 1 The postfix and prefix forms
+let a = 1, b = 1;
+
+let c = ++a; // 2 (prefix - returns new value)
+let d = b++; // 1 (postfix returns old value)
+                                
+alert (a); //2 
+alert (b); //2 
+alert (c); //2 
+alert (d); //1
+                                
+                              
