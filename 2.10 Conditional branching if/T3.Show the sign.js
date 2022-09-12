8ff7b0fef251182824861710f1158591f2ef55ba
malinovvski <m.malinovvski@gmail.com>
2022-09-12 18:06:31 +0200

    T3.Show te sign

---
+++ 2.10 Conditional branching if/T3.Show the sign.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Script</title>
+</head>
+<body>
+
+<script>
+    'use sctrict';
+
+let value = prompt("Type a number", '');
+ if ( value > 0 ) {
+    message = '1';
+ } else if (value < 0) {
+    message = '-1';
+ } else (value === 0 ) {
+    message = '0';
+ }
+
+
+</script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
