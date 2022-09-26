5516840bbd9aece934432c9e291e4686a97d17bf
malinovvski <100025958+malinovvski@users.noreply.github.com>
2022-09-26 17:15:55 +0200

    Update T2.Rewrite the function using.js

---
+++ 2.15 Functions/T2.Rewrite the function using.js
@@ -0,0 +1,15 @@
+
+
+//1.Question mark operator.
+
+  function checkAge(age) {
+    return (age > 18 ) ? true : confirm('Did parents allow you?');
+  }
+
+//2. Using OR || 
+
+    function checkAge(age){
+    return (age > 18) || confirm('Did parents allow you?');
+    }
+    
+   
